af9c29c34c39a32946fd220444f9ec1d1f411403
raowyr <pasquale.garzillo@gmail.com>
2016-03-24 14:58:12 +0100

    Lista categorie

--- Devevil.Blog.MVC.Client/Controllers/CategoryController.cs
+++ Devevil.Blog.MVC.Client/Controllers/CategoryController.cs
@@ -3,17 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Devevil.Blog.Model.Domain.Entities;
+using Devevil.Blog.MVC.Client.Models;
+using Devevil.Blog.Nhibernate.DAL.Base;
+using Devevil.Blog.Nhibernate.DAL.Repositories;
 
 namespace Devevil.Blog.MVC.Client.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController : BaseController
     {
         //
         // GET: /Category/
 
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                ListCategoryViewModel m = new ListCategoryViewModel();
+                using (UnitOfWork uow = new UnitOfWork())
+                {
+                    //Carica gli ultimi 5 POST
+                    PageRepository pr = new PageRepository(uow.Current);
+                    CategoryRepository cr = new CategoryRepository(uow.Current);
+
+                    IList<Page> pages = pr.GetTopPost(5);
+                    if (pages != null && pages.Count > 0)
+                    {
+                        int k = 0;
+                        foreach (var p in pages)
+                        {
+                            PostViewModel pTemp = new PostViewModel();
+
+                            pTemp.Id = p.Id;
+                            pTemp.Data = p.Date.Value;
+                            pTemp.Testo = p.BodyText;
+                            pTemp.Titolo = p.Title;
+                            pTemp.Autore = String.Format("{0} {1}", p.Author.Name, p.Author.Surname);
+                            pTemp.Categoria = p.Category.Name;
+
+                            m.PostPreview.Add(pTemp);
+
+                            k++;
+                        }
+                    }
+                    else
+                    {
+                        PostViewModel pTemp = new PostViewModel();
+
+                        pTemp.Id = 0;
+                        pTemp.Data = DateTime.Today;
+                        pTemp.Titolo = "OOPS...";
+                        pTemp.Testo = "Sembra non siano presenti articoli...";
+                        pTemp.Autore = "Pasquale Garzillo";
+
+                        m.PostPreview.Add(pTemp);
+                    }
+
+                    IList<Category> categories = cr.FindAll().ToList();
+                    if (categories != null && categories.Count > 0)
+                    {
+                        foreach (var c in categories)
+                        {
+                            CategoryViewModel cvm = new CategoryViewModel();
+                            cvm.Descrizione = c.Description;
+                            cvm.Nome = c.Name;
+                            cvm.FileName = c.ImageName;
+
+                            m.Categories.Add(cvm);
+                        }
+                    }
+                    else 
+                    {
+                        CategoryViewModel cvm = new CategoryViewModel();
+                        cvm.Descrizione = "Sembra non siano presenti articoli...";
+                        cvm.Nome = "OOPS...";
+
+                        m.Categories.Add(cvm);
+                    }
+                }
+                return View(m);
+            }
+            catch (Exception ex)
+            {
+                return Error(ex);
+            }
         }
 
     }
---
+++ Devevil.Blog.MVC.Client/Models/ListCategoryViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Devevil.Blog.MVC.Client.Models
+{
+    public class ListCategoryViewModel : BaseViewModel
+    {
+        private IList<PostViewModel> _postPreview;
+        private IList<CategoryViewModel> _categories;
+
+        private int _page;
+
+        public int Page
+        {
+            get { return _page; }
+            set { _page = value; }
+        }
+
+        public ListCategoryViewModel()
+        {
+            _postPreview = new List<PostViewModel>();
+            _categories = new List<CategoryViewModel>();
+        }
+
+        public IList<PostViewModel> PostPreview
+        {
+            get { return _postPreview; }
+            set { _postPreview = value; }
+        }
+
+        public IList<CategoryViewModel> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+
+            set
+            {
+                _categories = value;
+            }
+        }
+    }
+}
